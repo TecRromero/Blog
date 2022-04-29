4e63fa7a08b52969aaab423aec3c5450b4139cab
Rudy Romero <TecRRomero@gmail.com>
2022-04-29 14:34:24 -0400

    eidt tailwind conf

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  get 'home', to:'home#index'
+  get 'home', to: 'home#index'
   root 'public#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
