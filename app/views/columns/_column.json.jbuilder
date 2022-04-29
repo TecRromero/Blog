a9723c2243aec2418511c842859acd92c9eaef1f
Rudy Romero <TecRRomero@gmail.com>
2022-04-29 17:13:41 -0400

    add crud for the blog

---
+++ app/views/columns/_column.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! column, :id, :user_id, :body, :created_at, :updated_at
+json.url column_url(column, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :columns
   devise_for :users
   get 'home', to: 'home#index'
   root 'public#index'
